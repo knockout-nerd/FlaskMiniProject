8f2ae1f3341fef23ebdd491c5b2a42350721d55a
Stanley Adumuah <stancode3271@gmail.com>
2024-05-09 18:08:59 +0000

    added unfinished changes

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/homepage')
+@app.route('/Home')
 def homepage():
     return render_template('index.html')
 
@@ -10,7 +10,14 @@ def homepage():
 def about_page():
     return render_template('about.html')
 
+@app.route('/create', methods=["GET","POST"])
+def create_post():
+    return render_template('posts/create.html')
 
+@app.route('/posts')
+def posts():
+    posts = []
+    return render_template('posts/posts.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
